b6be9dfc874c6284584034aa07f4626654084646
Sara Cook <saracook087@gmail.com>
2022-01-21 17:13:41 -0500

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main>
+      <h1>Homepage</h1>
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/Play.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Play = () => {
+  return (
+    <>
+      <h1>Toe</h1>
+    </>
+  );
+};
+
+export default Play;
