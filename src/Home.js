994b8804fcf8c5681e5edeb0cf1d93065347e052
Sara Cook <saracook087@gmail.com>
2022-01-23 18:54:05 -0500

    Added Board, Square, and helpers components

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import illusion from "./optical-illusions-abstract.gif";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <main>
-      <h1>Homepage</h1>
+      <div className="homepage">
+        <Link to="/play" className="playButton">
+          Play!
+        </Link>
+        <img src={illusion} alt="optical-illusion" className="illusion" />
+      </div>
     </main>
   );
 };
--- src/Play.js
+++ src/Play.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Board from "./Board";
 
 const Play = () => {
+  const handleClick = () => {
+
+  }
+
+  const jumpTo = () => {
+
+  }
+
+  const renderMoves = () => {
+
+  }
+
   return (
     <>
-      <h1>Toe</h1>
+      <div className="container">
+        <h1>Tic Tac Toe Game</h1>
+        <Board onClick={handleClick}/>
+      </div>
     </>
   );
 };
